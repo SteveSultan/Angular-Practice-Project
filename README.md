75567e6be3ccc22d750a3b6f7b8e34fff706b92d
SteveSultan <stevesultan@outlook.com>
2020-11-30 13:14:43 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-comp229
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-comp229)
---
+++ src/app/products.ts
@@ -0,0 +1,33 @@
+export const products = [
+  {
+    id:"1",
+    name: 'iPhone 12mini ',
+    price: 799,
+    description: 'A large phone with one of the best screens'
+  },
+  {
+    id:"2",
+    name: 'iPhone 11',
+    price: 699,
+    description: 'A great phone with one of the best cameras'
+  },
+  {
+    id:"3",
+    name: 'iPhone SE',
+    price: 299,
+    description: 'cheapest iPhone ever'
+  },
+   {
+     id:"4",
+    name: 'Google Pxiel 3',
+    price: 299,
+    description: 'New product of google'
+  }
+];
+
+
+/*
+Copyright Google LLC. All Rights Reserved.
+Use of this source code is governed by an MIT-style license that
+can be found in the LICENSE file at https://angular.io/license
+*/
